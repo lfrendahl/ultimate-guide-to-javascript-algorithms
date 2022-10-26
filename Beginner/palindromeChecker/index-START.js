6ade7183b13f597aa23b7a81bf4ddba0468213c4
lfrendahl <lfrendahl3@gmail.com>
2022-10-26 11:00:22 -0400

    lesson 8

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -3,12 +3,40 @@ Given a string of text, return true or false indicating whether or not the text 
 e.g palindromeChecker('racecar') // will return true
 */
 
-
-
-
+//Not the best becauase it has to run two loops
 function palindromeChecker(text) {
-    v// Code goes here
+    let reversedText = text.toLowerCase().split('').reverse().join('')
+    return (reversedText === text.toLowerCase())
 }
+
+
+//We check the character array. Is every letter at an index equal to the letter at that negative index? To find the negative for 0 it is -1 and for 1 it is -2, so we adjust this in our function.
+function palindromeChecker(text) {
+ let charArray = text.toLowerCase().split('')
+
+ let result = charArray.every((letter, index) => {
+    return letter === charArray[charArray.length - 1 - index]
+ })
+
+ return result
+
+}
+
+//We only need to loop halfway through 
+function palindromeChecker(text) {
+
+
+    for (let i=0; i < text.length/2; i++) {
+       if (text[i] !== text[text.length -1 - i]) {
+        return false
+       }
+       return true
+    }
+   }
+   
+
+
+
 
 
 
