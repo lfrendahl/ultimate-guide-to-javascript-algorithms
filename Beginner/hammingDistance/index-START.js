716231aaa777d8a3a9dcd1165a6782f61e8d34ce
lfrendahl <lfrendahl3@gmail.com>
2022-10-26 11:33:15 -0400

    lesson 9

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -5,9 +5,22 @@ E.g hammingDistance('rover', 'river') // should return 1
 
 
 
-
+//Only run the code if the strings are the same length. Loop through and on every loop if they are not equal increment the result counter.
 function hammingDistance(stringA, stringB) {
-    // Code goes here
+  let result = 0
+
+  if (stringA.length == stringB.length) {
+
+    for (let i=0; i < stringA.length; i++) {
+        if (stringA[i].toLowerCase() != stringB[i].toLowerCase()) {
+            result++
+        }
+    }
+
+  } else {
+    throw new Error('Strings do not have equal length')
+  }
+  return result
 }
 
 
