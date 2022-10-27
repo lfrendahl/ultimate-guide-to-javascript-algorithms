2f00b1598ee442383cb89d9546bb89e607d41d25
lfrendahl <lfrendahl3@gmail.com>
2022-10-27 11:29:01 -0400

    lesson 12

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -5,11 +5,52 @@ don't. E.g
     isAnagram('silent', 'listen') // should return true
 */
 
-
-
-function isAnagram(stringA, stringB) {
-    // Code goes here
+const sanitizeString = function (str) {
+    return str.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('');
 }
+
+//Mine - it works
+function isAnagram(stringA, stringB) {
+   if (stringA.length != stringB.length) {
+    return false
+   }
+   let newA = stringA.split('').sort().join('')
+   let newB = stringB.split('').sort().join('')
+   console.log(newA, newB)
+   return newA == newB
+}
+
+//Creates two charMaps and compares them.
+function isAnagram(stringA, stringB) {
+
+    function createCharMap(text) {
+        let charMap = {}
+
+        for (let char of text) {
+            if (charMap.hasOwnProperty(char)) {
+                charMap[char]++
+            } else {
+                charMap[char] = 1
+            }
+        }
+
+        return charMap
+    }
+
+    if (stringA.length === stringB.length) {
+        let stringAMap = createCharMap(stringA)
+        let stringBMap = createCharMap(stringB)
+        for (let char in stringAMap) {
+            if (stringAMap[char] !== stringBMap[char]) {
+                return false
+            }
+        }
+        return true
+    } else {
+        return false
+    }
+}
+
 
 
 module.exports = isAnagram
